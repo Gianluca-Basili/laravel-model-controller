23f70e39d320e5578eca7951242da4727230ac05
Gianluca-Basili <gianluc.basili@gmail.com>
2023-07-26 17:22:37 +0200

    esercizio finito

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,12 +2,29 @@
 
 @section('content')
    <div class="container">
-    <div class="row">
+    <div class="row my-5">
         <div class="col-12 col-md-6 col-lg-4">
             <img class="img-fluid" src="{{$comic->thumb}}" >
         </div>
         <div class="col-12 col-md-6 col-lg-4">
             <h2>{{$comic->title}}</h2>
+            <p>Prezzo:{{$comic->price}}</p>
+            <p>Serie:{{$comic->series}}</p>
+            <p>Data di uscita:{{$comic->sale_date}}</p>
+        </div>
+           
+        <div class="row">
+            <div class="col-12">
+                <p>{{$comic->description}}</p>
+            </div>
+            <div class="col-12 col-md-6">
+                Autori:{{$comic->writers}}
+              
+            </div>
+            <div class="col-12 col-md-6">
+                Autori:{{$comic->artists}}
+               
+            </div>
         </div>
 
     </div>
